a7959bee45475f3737ed3442bc54fdd3e75802a1
NicolaL95 <nicola.lilla@gmail.com>
2021-10-25 16:00:33 +0200

    Creazione oggetti

---
+++ main.js
@@ -0,0 +1,61 @@
+let players = [
+    {
+        id: 1,
+
+        contenuto: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Eveniet inventore labore laudantium rerum quidem autem temporibus eum obcaecati facere nulla.",
+
+        immagine: "https://unsplash.it/300/300?image=",
+
+
+        autore: ["https://unsplash.it/300/300?image=", "nomecognome1"],
+
+        numeroLikes = 20,
+
+        dataCreazione = "19/10/2021"
+    },
+
+    {
+        id: 2,
+
+        contenuto: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Eveniet inventore labore laudantium rerum quidem autem temporibus eum obcaecati facere nulla.",
+
+        immagine: "https://unsplash.it/300/300?image=",
+
+
+        autore: ["https://unsplash.it/300/300?image=", "nomecognome2"],
+
+        numeroLikes = 0,
+
+        dataCreazione = "11/10/2020"
+    },
+
+    {
+        id: 3,
+
+        contenuto: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Eveniet inventore labore laudantium rerum quidem autem temporibus eum obcaecati facere nulla.",
+
+        immagine: "https://unsplash.it/300/300?image=",
+
+
+        autore: ["https://unsplash.it/300/300?image=", "nomecognome3"],
+
+        numeroLikes = 10,
+
+        dataCreazione = "12/10/2020"
+    },
+
+    {
+        id: 4,
+
+        contenuto: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Eveniet inventore labore laudantium rerum quidem autem temporibus eum obcaecati facere nulla.",
+
+        immagine: "https://unsplash.it/300/300?image=",
+
+
+        autore: ["https://unsplash.it/300/300?image=", "nomecognome4"],
+
+        numeroLikes = 30,
+
+        dataCreazione = "10/10/2020"
+    },
+]
